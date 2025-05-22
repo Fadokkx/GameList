fdf14433e4218ba7423350b78e86c50353d9ec7a
Felipe Apolinário <Apolinario.F.P@gmail.com>
2025-05-22 00:03:26 -0300

    GamenMinDTO; GService; GController; GRepository

---
+++ src/main/java/com/fapolinario/dslist/services/GameService.java
@@ -0,0 +1,23 @@
+package com.fapolinario.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fapolinario.dslist.dto.GameMinDTO;
+import com.fapolinario.dslist.entities.Game;
+import com.fapolinario.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+	}
+}
