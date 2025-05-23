a9b99df377e45bade0b0c6f808d4cd1364ff7d59
Felipe Apolinário <Apolinario.F.P@gmail.com>
2025-05-23 13:52:54 -0300

    List/Id/games

--- src/main/java/com/fapolinario/dslist/dto/GameMinDTO.java
+++ src/main/java/com/fapolinario/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.fapolinario.dslist.dto;
 
 import com.fapolinario.dslist.entities.Game;
+import com.fapolinario.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	
@@ -14,12 +15,19 @@ public class GameMinDTO {
 	}
 
 	public GameMinDTO(Game entity) {
-		super();
 		id = entity.getId();
 		title = entity.getTitle();
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
--- src/main/java/com/fapolinario/dslist/services/GameService.java
+++ src/main/java/com/fapolinario/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.fapolinario.dslist.dto.GameDTO;
 import com.fapolinario.dslist.dto.GameMinDTO;
 import com.fapolinario.dslist.entities.Game;
+import com.fapolinario.dslist.projections.GameMinProjection;
 import com.fapolinario.dslist.repositories.GameRepository;
 
 @Service
@@ -20,15 +21,18 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public GameDTO findById(Long id) {
 		Game result = gameRepository.findById(id).get();
-		GameDTO dto = new GameDTO(result);
-		return dto;
-		
+		return new GameDTO(result);	
 	}
 	
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
-		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
-		return dto;
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
